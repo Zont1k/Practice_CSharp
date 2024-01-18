a1372fa2f0ef50289788cc4bb1fe106317ed0ef8
Oleksiii <oleksiizotov06@gmail.com>
2024-01-18 21:31:45 +0100

    tasks were refactored

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -2,27 +2,27 @@
 {
     static void Main(string[] args)
     {
-        Random random = new Random();
-
-        int minRange = 1;
-        int maxRange = 100;
+        const int minRange = 1;
+        const int maxRange = 100;
 
         int[] numbers = new int[10];
+
+        FillArrayWithRandomNumbers(numbers, minRange, maxRange);
+
+        var sum = numbers.Sum();
+
+        var avg = numbers.Average();
+
+        Console.WriteLine($"Summa: {sum}. Srednie arifmeticheskoe: {avg}");
+    }
+
+    private static void FillArrayWithRandomNumbers(int[] numbers, int minRange, int maxRange)
+    {
+        Random random = new Random();
 
         for (int i = 0; i < numbers.Length; i++)
         {
             numbers[i] = random.Next(minRange, maxRange);
         }
-
-        int sum = 0;
-       
-        foreach (int i in numbers)
-        {
-            sum += i;
-        }
-
-        double sredArif = sum / numbers.Length;
-
-        Console.WriteLine($"Summa: {sum}. Srednie arifmetichesoe: {sredArif}");
     }
 }
--- Lesson_3/Program.cs
+++ Lesson_3/Program.cs
@@ -1,42 +1,114 @@
-﻿class Program
+﻿namespace Lesson_3
 {
-    static void Main(string[] args)
+    class Program
     {
-        Random random  = new Random();
-
-        int minNumber = 1;
-        int maxNumber = 10;
-
-        int[,] array = new int[5, 5];
-
-        for (int i = 0; i < array.GetLength(0); i++)
+        static void Main(string[] args)
         {
-            for(int j = 0;  j < array.GetLength(1); j++)
+            Random random = new Random();
+
+            int minNumber = 1;
+            int maxNumber = 10;
+
+            int[,] array = new int[5, 5];
+
+            // Todo: "new Method"
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                array[i, j] = random.Next(minNumber, maxNumber);
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(minNumber, maxNumber);
+                }
             }
-        }
 
-        Console.WriteLine("Default matrix:/n");
+            Console.WriteLine("Default matrix:/n");
 
-        for (int i = 0; i < array.GetLength(0); i++) 
-        {
-            for(int j = 0; j < array.GetLength(1); j++)
+            // Todo: "new Method"
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                Console.Write($"{array[i, j]} \t");
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]} \t");
+                }
+                Console.WriteLine();
             }
-            Console.WriteLine(); 
-        }
 
-        Console.WriteLine("Not Default matrix:/n");
+            Console.WriteLine("Not Default matrix:/n");
 
-        for (int j = 0; j < array.GetLength(1);j++)
-        {
-            for(int i = 0; i < array.GetLength(0);i++)
+            // Todo: "Create new Method for transposing"
+            // Todo: "Call Display Method for transpost Matrix"
+            for (int j = 0; j < array.GetLength(1); j++)
             {
-                Console.Write($"{array[i, j]} \t");
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    Console.Write($"{array[i, j]} \t");
+                }
+                Console.WriteLine();
             }
-            Console.WriteLine();
         }
     }
 }
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+
+        const int minNumber = 1;
+        const int maxNumber = 10;
+
+        int[,] array = new int[5, 5];
+
+        FillMatrixWithRandomValues(array, random, minNumber, maxNumber);
+
+        Console.WriteLine("Default matrix:\n");
+        DisplayMatrix(array);
+
+        Console.WriteLine("Transposed matrix:\n");
+        int[,] transposedArray = TransposeMatrix(array);
+        DisplayMatrix(transposedArray);
+    }
+
+    static void FillMatrixWithRandomValues(int[,] matrix, Random random, int minValue, int maxValue)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = random.Next(minValue, maxValue);
+            }
+        }
+    }
+
+    static void DisplayMatrix(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write($"{matrix[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+    static int[,] TransposeMatrix(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        int[,] transposedMatrix = new int[cols, rows];
+
+        for (int j = 0; j < cols; j++)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                transposedMatrix[j, i] = matrix[i, j];
+            }
+        }
+
+        return transposedMatrix;
+    }
+}
+
