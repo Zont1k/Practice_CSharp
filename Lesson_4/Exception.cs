f710625f6de1449e08feccf0c33403d89e62218e
Oleksii Zotov <82260790+Zont1k@users.noreply.github.com>
2024-02-08 23:23:05 +0100

    the task is completed (#2)
    * the task is completed
    
    * deficiencies have been corrected
    
    * the code has been enhanced
    
    * done

---
+++ Lesson_4/Exception.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4_Exceptions
+{
+    public class UserInputException : Exception
+    {
+        public UserInputException(string message) : base(message) { }
+    }
+
+    public class InvalidNameException : UserInputException
+    {
+        public InvalidNameException(string message) : base(message) { }
+    }
+
+    public class InvalidIntException : UserInputException
+    {
+        public InvalidIntException(string message) : base(message) { }
+    }
+
+    public class InvalidDoubleException : UserInputException
+    {
+        public InvalidDoubleException(string message) : base(message) { }
+    }
+}
