062b6f4cb6839586b28657911a44c083664e65da
Oleksii Zotov <oleksiizotov06@gmail.com>
2024-04-08 22:25:17 +0200

    done (#4)
    * done
    
    * test
    
    * two new methods have been added

---
+++ Lesson_8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> intList = new LinkedList<int>();
+
+        intList.Add(1);
+        intList.Add(2);
+        intList.Add(3);
+
+        foreach (var item in intList)
+        {
+            Console.WriteLine(item);
+        }
+
+        LinkedList<string> stringList = new LinkedList<string>();
+
+        stringList.Add("Hello");
+        stringList.Add("World");
+        stringList.Add("!");
+
+        for (int i = 0; i < stringList.Count; i++)
+        {
+            Console.WriteLine(stringList[i]);
+        }
+    }
+}
