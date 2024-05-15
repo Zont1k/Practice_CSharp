64d7971f97c6bfc58a3a945e35db49adb69b3108
Oleksii <oleksiizotov06@gmail.com>
2024-05-15 23:54:35 +0200

    feat: the array sorting method has been created

---
+++ Lesson_10_Algorithm/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 4, 9, 7, 6, 2, 3, 8, 5, 0, 1 };
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[i] > array[j])
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+        PrintArray(array);
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (var item in arr)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
