486b99f34b99794cb0f46d25246bcd915066dc51
Oleksii <oleksiizotov06@gmail.com>
2024-05-20 23:15:06 +0200

    feat: renamed mathod; introduced a variable for array[i] and moved out it from this loop; removed additional variable and added addition and subtraction method; created a new method for implementing there another sorting algorithm.

--- Lesson_10_Algorithm/Program.cs
+++ Lesson_10_Algorithm/Program.cs
@@ -1,34 +1,74 @@
-﻿using System;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        int[] array = { 4, 9, 7, 6, 2, 3, 8, 5, 0, 1 };
+        int[] array = { 9, 4, 7, 6, 2, 3, 8, 5, 0, 1 };
 
-        SortMathod(array);
+        BubbleSortMethod(array);
 
-        PrintArray(array);
+        PrintArray("BubbleSort:", array);
+
+        QuickSortMethod(array, 0, array.Length - 1);
+
+        PrintArray("QuickSort:", array);
     }
 
-    private static void SortMathod(int[] array)
+    public static void BubbleSortMethod(int[] array)
     {
-        for (int i = 0; i < array.Length - 1; i++)
+        for (int i = 0; i < array.Length; i++)
         {
+            int currentElement = array[i];
+
             for (int j = i + 1; j < array.Length; j++)
             {
-                if (array[i] > array[j])
+                if (currentElement > array[j])
                 {
-                    int temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
+                    currentElement = currentElement + array[j];
+                    array[j] = currentElement - array[j];
+                    currentElement = currentElement - array[j];
                 }
             }
+            array[i] = currentElement;
+        }
+    }
+    public static void QuickSortMethod(int[] array, int low, int high)
+    {
+        if (low < high)
+        {
+            int partitionIndex = Partition(array, low, high);
+
+            QuickSortMethod(array, low, partitionIndex - 1);
+            QuickSortMethod(array, partitionIndex + 1, high);
         }
     }
 
-    static void PrintArray(int[] arr)
+    public static int Partition(int[] array, int low, int high)
     {
+        int pivot = array[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++)
+        {
+            if (array[j] < pivot)
+            {
+                i++;
+
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        int temp1 = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp1;
+
+        return i + 1;
+    }
+
+    static void PrintArray(string nameSortMethod, int[] arr)
+    {
+        Console.WriteLine($"{nameSortMethod}");
         foreach (var item in arr)
         {
             Console.WriteLine(item);
