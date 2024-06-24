0cd0aa5d0459b80d2c60f8280efdd771d5968fff
Oleksii Zotov <oleksiizotov06@gmail.com>
2024-06-24 19:20:15 +0200

    feat: added server-side logic for handling requests and sending the time zone; (#9)
    Added frontend for displaying the received time zone to the user.

---
+++ TimeZoneServerWebApi/Controllers/TimeZoneController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace TimeZoneServerWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TimeController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult GetTime([FromBody] string timeZoneId)
+        {
+            if (string.IsNullOrEmpty(timeZoneId))
+            {
+                return BadRequest("Id of time zone can't be empty");
+            }
+
+            try
+            {
+                TimeZoneInfo timeZone = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+                DateTime currentTime = TimeZoneInfo.ConvertTime(DateTime.UtcNow, timeZone);
+                string responseString = $"Current time in this time zone {timeZoneId}: {currentTime}";
+
+                return Ok(responseString);
+            }
+            catch (TimeZoneNotFoundException)
+            {
+                return BadRequest("Not a valid time zone Id");
+            }
+        }
+    }
+}
